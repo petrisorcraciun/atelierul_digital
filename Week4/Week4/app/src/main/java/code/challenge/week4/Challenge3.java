1f170c512eba0342808ad53577ab5e7cdda7733d
Crăciun Petrișor <petrisor.craciun34@gmail.com>
2021-01-24 09:44:54 +0200

    add challenge 1 + 3 week 4

---
+++ Week4/Week4/app/src/main/java/code/challenge/week4/Challenge3.java
@@ -0,0 +1,14 @@
+package code.challenge.week4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Challenge3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge3);
+    }
+}
